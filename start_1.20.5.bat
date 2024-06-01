b86de4fa0178449698cf72cc384b8e8cff48d977
nssuperx <40332687+nssuperx@users.noreply.github.com>
2024-06-01 15:58:18 +0900

    1.20.5

---
+++ start_1.20.5.bat
@@ -0,0 +1,8 @@
+@echo off
+rem set java path
+set JAVA_HOME=%LOCALAPPDATA%\packages\microsoft.4297127d64ec6_8wekyb3d8bbwe\localcache\local\runtime\java-runtime-delta\windows-x64\java-runtime-delta
+
+rem run
+%JAVA_HOME%\bin\java.exe -Xmx1024M -Xms1024M -jar server.jar nogui
+
+pause
---
+++ start_1.20.5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARCH="x64"
+# ARCH="aarch64"
+
+# setup java runtime
+JAVACMD=`find . -type f -path "./jdk/bin/java"`
+if [ ! -x "$JAVACMD" ] ; then
+    wget https://corretto.aws/downloads/latest/amazon-corretto-21-${ARCH}-linux-jdk.tar.gz -O jdk.tar.gz
+    mkdir jdk
+    tar -xvf jdk.tar.gz -C jdk --strip-components=1
+    JAVACMD=`find . -type f -path "./jdk/bin/java"`
+    rm jdk.tar.gz
+fi
+
+# run
+$JAVACMD -Xmx1024M -Xms1024M -jar server.jar nogui
